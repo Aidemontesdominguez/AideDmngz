6ba79d6cf9f57b09f29c7a88f13fc039d487c623
Aidemontesdominguez <60208133+Aidemontesdominguez@users.noreply.github.com>
2020-01-23 19:48:01 -0600

    Add files via upload

---
+++ 20190822_P001_HolaMundo.cpp
@@ -0,0 +1,11 @@
+/* Autor: Montes Dominguez Aide Montserrat
+    Descripción: Primer Programa Hola Mundo
+    Fecha: 22 de agosto de 2019
+    */
+
+#include <iostream>
+
+int main()
+{
+    std::cout<< "Hello world!";
+}
